3a9c2ce075d240c3efea165d3dfb0450f52a74c4
Jerome Chavez <jayrome12@yahoo.com>
2018-06-13 13:05:23 -0700

    Created styles.css with classes adjust the rotation of the img based on north, south, west or east. Linked styles.css to index.html and added west class to img element to test

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var carImage = document.createElement('img')
 carImage.setAttribute('src', 'https://openclipart.org/download/234444/stripes.svg')
 carImage.setAttribute('width', '100')
 carImage.setAttribute('height', '50')
+carImage.classList.add('west')
 document.body.appendChild(carImage)
 
 var car1 = new Car(carImage, 5, 'east', [0, 0])
